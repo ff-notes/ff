9a53440d643112f7db55ecd507dbf92758269203
Yuriy Syrovetskiy <cblp@cblp.su>
2018-01-05 11:32:53 +0300

    implement agenda in exe

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.ByteString as BS
+import           Data.Yaml (ToJSON, object, (.=))
+import qualified Data.Yaml.Pretty as Yaml
+import           System.Directory (getHomeDirectory)
+import           System.FilePath ((</>))
+
+import           FF (Result (Result), notes, runAgenda)
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    home <- getHomeDirectory
+    let notesDir = home </> "Yandex.Disk.localized/Apps/ff"
+    Result{notes} <- runAgenda notesDir
+    yprint $ object ["agenda" .= notes]
+
+yprint :: ToJSON a => a -> IO ()
+yprint = BS.putStr . Yaml.encodePretty Yaml.defConfig
--- lib/FF.hs
+++ lib/FF.hs
@@ -12,7 +12,7 @@ import           CRDT.LamportClock (LamportTime, Pid)
 import           CRDT.LWW (LWW)
 import qualified CRDT.LWW as LWW
 import           Data.Aeson (eitherDecode)
-import           Data.Aeson.TH (defaultOptions, deriveJSON)
+import           Data.Aeson.TH (defaultOptions, deriveJSON, deriveToJSON)
 import qualified Data.ByteString.Lazy as BS
 import           Data.List.NonEmpty (nonEmpty)
 import           Data.Map.Strict (Map)
@@ -37,25 +37,31 @@ type NoteId = Text
 
 type NoteView = Text
 
-data Result = Result
+newtype Result = Result
     { notes :: Map NoteId NoteView
     }
     deriving (Eq, Show)
 
+deriveToJSON defaultOptions ''Result
+
 noteView :: Note -> NoteView
 noteView (Note lww) = LWW.query lww
+
+loadDocument :: FilePath -> FilePath -> IO (Maybe Note)
+loadDocument base doc = do
+    versionFiles <- listDirectory $ base </> doc
+    versions <- for versionFiles $ \version -> do
+        let versionPath = base </> doc </> version
+        contents <- BS.readFile versionPath
+        pure $
+            either (error . ((versionPath ++ ": ") ++)) id $
+            eitherDecode contents
+    pure $ sconcat <$> nonEmpty versions
 
 runAgenda :: FilePath -> IO Result
 runAgenda base = do
     files <- listDirectory $ base </> "note"
     mnotes <- for files $ \doc -> do
-        versionFiles <- listDirectory $ base </> "note" </> doc
-        versions <- for versionFiles $ \version -> do
-            let versionPath = base </> "note" </> doc </> version
-            contents <- BS.readFile versionPath
-            let note =
-                    either (error . ((versionPath ++ ": ") ++)) id $
-                    eitherDecode contents
-            pure note
-        pure (Text.pack doc, noteView . sconcat <$> nonEmpty versions)
+        note <- loadDocument (base </> "note") doc
+        pure (Text.pack doc, noteView <$> note)
     pure Result{notes = Map.fromList [(k, note) | (k, Just note) <- mnotes]}
