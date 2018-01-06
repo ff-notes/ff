a902a3fdba8c7b4f7fe3f085e7f96d766f7e37d9
kemphack <kemphack@mail.ru>
2018-01-06 18:56:21 +0300

    firsttry

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -12,17 +13,28 @@ import qualified Data.ByteString as BS
 import qualified Data.Map.Strict as Map
 import           Data.Semigroup ((<>))
 import           Data.Text (Text)
-import           Data.Yaml (ToJSON, object, (.=))
+import           Data.Yaml (ToJSON, FromJSON, object, (.=), encodeFile, decodeFileEither)
 import qualified Data.Yaml.Pretty as Yaml
+import           GHC.Generics (Generic)
 import           Options.Applicative (ParserInfo, command, execParser, fullDesc,
                                       helper, info, metavar, progDesc,
                                       strArgument, subparser, (<**>))
 import           System.Directory (getHomeDirectory)
-import           System.FilePath ((</>))
+import           System.FilePath ((</>), FilePath)
 
 import           FF (DocId (DocId), cmdAgenda, cmdDone, cmdNew)
 
-data Cmd = Agenda | Done DocId | New Text
+data Cmd = Agenda | Done DocId | New Text | Dir FilePath
+
+data Config = Config{
+    dataDir :: FilePath
+} deriving Generic
+
+instance FromJSON Config
+instance ToJSON Config
+
+cfgFile :: FilePath
+cfgFile = "cfg.yaml"
 
 cmdInfo :: ParserInfo Cmd
 cmdInfo =
@@ -34,31 +46,48 @@ cmdInfo =
             [ command' "agenda" cmdAgendaParser
             , command' "done"   cmdDoneParser
             , command' "new"    cmdNewParser
+            , command' "dir"    cmdDirParser
             ])
         <|> cmdAgendaParser
     cmdAgendaParser = pure Agenda
     cmdDoneParser   = Done . DocId  <$> strArgument (metavar "ID")
     cmdNewParser    = New           <$> strArgument (metavar "TEXT")
+    cmdDirParser    = Dir           <$> strArgument (metavar "DIRECTORY")
     command' name parser = command name $ info (parser <**> helper) fullDesc
 
 main :: IO ()
 main = do
-    home <- getHomeDirectory
-    let dataDir = home </> "Yandex.Disk.localized/Apps/ff"
+    ecfg <- decodeFileEither cfgFile
     cmd <- execParser cmdInfo
-    timeVar <- newTVarIO =<< getRealLocalTime
-    runLamportClock timeVar $ runCmd dataDir cmd
+    case ecfg of
+        Right cfg ->
+            runCmd (Just cfg) cmd
+        Left _    ->
+            runCmd Nothing cmd
 
-runCmd :: FilePath -> Cmd -> LamportClock ()
-runCmd dataDir Agenda = liftIO $ do
-    agenda <- cmdAgenda dataDir
-    if null agenda then putStrLn "nothing" else yprint agenda
-runCmd dataDir (New text) = do
-    noteId <- cmdNew dataDir text
-    yprint $ Map.singleton noteId text
-runCmd dataDir (Done noteId) = do
-    text <- cmdDone dataDir noteId
-    yprint $ object ["archived" .= Map.singleton noteId text]
+runCmd :: Maybe Config -> Cmd -> IO ()
+runCmd mcfg cmd =
+    case mcfg of
+        Just cfg ->
+            let dir = dataDir cfg in
+            case cmd of
+                Dir newDir ->
+                    encodeFile cfgFile (cfg{dataDir = newDir})
+                Agenda -> do
+                    agenda <- cmdAgenda dir
+                    if null agenda then putStrLn "nothing" else yprint agenda
+                Done noteId -> do
+                    text <- cmdDone dir noteId
+                    yprint $ object ["archived" .= Map.singleton noteId text]
+                New text -> do
+                    noteId <- cmdNew dir text
+                    yprint $ Map.singleton noteId text
+        Nothing  ->
+            case cmd of
+                Dir dir ->
+                    encodeFile cfgFile (Config dir)
+                _       ->
+                    putStrLn "Working directory isn't specified, use *dir* command to specify it"
 
 yprint :: (ToJSON a, MonadIO io) => a -> io ()
 yprint = liftIO . BS.putStr . Yaml.encodePretty Yaml.defConfig
