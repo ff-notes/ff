64d3a534ff75467fdce07ba0fc1b9297bdd19729
Yuriy Syrovetskiy <cblp@cblp.su>
2018-01-26 18:05:42 +0300

    Make note fields a bit more compact

--- lib/FF/UI.hs
+++ lib/FF/UI.hs
@@ -11,19 +11,22 @@ import           Data.Text (Text)
 import qualified Data.Text as Text
 import           Data.Time (Day)
 import           Numeric.Natural (Natural)
-import           Text.PrettyPrint.Mainland (Doc, brackets, hang, indent, sep,
-                                            stack, star, strictText, (<+/>),
-                                            (</>), (<>))
+import           Text.PrettyPrint.Mainland (Doc, brackets, commasep, hang,
+                                            indent, sep, stack, star,
+                                            strictText, (<+/>), (</>), (<>),
+                                            (<|>))
 import qualified Text.PrettyPrint.Mainland as Pretty
 import           Text.PrettyPrint.Mainland.Class (Pretty, ppr)
 
 import           FF.Types (Agenda (..), NoteView (..), Sample (..))
 
-(.=) :: Pretty a => Doc -> a -> Doc
-label .= value = hang indentation $ label `mappend` ":" <+/> ppr value
+type Template a = a -> String
 
-withHeader :: Pretty a => Doc -> a -> Doc
-withHeader header value = hang indentation $ header </> ppr value
+(.=) :: Pretty a => String -> a -> Doc
+label .= value = hang indentation $ Pretty.text label <+/> ppr value
+
+withHeader :: Pretty a => String -> a -> Doc
+withHeader header value = hang indentation $ Pretty.text header </> ppr value
 
 indentation :: Int
 indentation = 4
@@ -38,13 +41,11 @@ agenda :: Int -> Agenda -> Doc
 agenda limit Agenda{ending, starting} = stack
     [ sample labelEnding   "ff search --started --ending" ending
     , sample labelStarting "ff search --starting"         starting
-    , "to see more tasks, run" .= ("ff --limit=" <> pshow (max 0 limit + 10))
+    , "to see more tasks, run:" .= ("ff --limit=" <> show (max 0 limit + 10))
     ]
   where
-    labelEnding   count = pshow count <> " started task(s) ending soon:"
-    labelStarting count = pshow count <> " task(s) starting soon:"
-
-type Template a = a -> Doc
+    labelEnding   n = show n <> " started task(s) ending soon:"
+    labelStarting n = show n <> " task(s) starting soon:"
 
 sample :: Template Natural -> Text -> Sample -> Doc
 sample labelTemplate cmdToSeeAll Sample{notes, total} = stack $
@@ -52,7 +53,7 @@ sample labelTemplate cmdToSeeAll Sample{notes, total} = stack $
     : [toSeeAllLabel .= strictText cmdToSeeAll | count /= total]
   where
     count = genericLength notes
-    toSeeAllLabel = "to see all " <> pshow total <> " task(s), run"
+    toSeeAllLabel = "to see all " <> show total <> " task(s), run:"
 
 instance Pretty NoteView where
     ppr NoteView{..} =
@@ -61,6 +62,7 @@ instance Pretty NoteView where
                 star
                 : map strictText (Text.words text)
                 ++ [brackets $ pshow nid])
-        </> indent
-            indentation
-            (stack $ "start" .= start : ["end" .= e | Just e <- pure end])
+        </> indent indentation (fieldsSep fields)
+      where
+        fields = "start" .= start : ["end" .= e | Just e <- pure end]
+        fieldsSep docs = commasep docs <|> stack docs
