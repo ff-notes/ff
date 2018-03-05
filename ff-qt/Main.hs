ef521b2a0360c00fdd2dc90a9114523961fb04ac
Yuriy Syrovetskiy <cblp@cblp.su>
2018-03-05 19:33:55 +0300

    Use QToolBox

--- ff-qt/Main.hs
+++ ff-qt/Main.hs
@@ -1,69 +1,70 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
-import           Control.Concurrent.STM (newTVarIO)
+import           Control.Concurrent.STM (TVar, newTVarIO)
 import           Control.Monad (void)
-import           CRDT.LamportClock (getRealLocalTime)
+import           CRDT.LamportClock (LocalTime, getRealLocalTime)
 import           Data.Foldable (traverse_)
+import qualified Data.Text as Text
 import           Data.Version (showVersion)
 import           Foreign.Hoppy.Runtime (withScopedPtr)
-import           QAbstractItemView (setAlternatingRowColors, setCurrentIndex,
-                                    setItemDelegate, setModel)
 import           QApplication (QApplication, new)
+import           QBoxLayout (addWidget)
 import           QCloseEvent (QCloseEvent)
 import           QCoreApplication (exec, setApplicationName,
                                    setApplicationVersion, setOrganizationDomain,
                                    setOrganizationName)
+import           QLabel (QLabel, newWithText)
 import           QMainWindow (QMainWindow, new, restoreState, saveState,
                               setCentralWidget)
 import           QSettings (new, setValue, value)
 import           QShowEvent (QShowEvent)
-import           QStandardItem (index)
-import           QStandardItemModel (item)
-import           QStyledItemDelegate (new)
 import           QTabWidget (QTabWidget, addTab, new)
 import           Qtah.Event (onEvent)
-import           QTreeView (QTreeView, expandAll, new, setHeaderHidden)
+import           QToolBox (QToolBox, addItem, new)
 import           QVariant (newWithByteArray, toByteArray)
-import           QWidget (QWidgetPtr, restoreGeometry, saveGeometry, setFocus,
+import           QVBoxLayout (newWithParent)
+import           QWidget (QWidgetPtr, new, restoreGeometry, saveGeometry,
                           setWindowTitle, show)
 import           System.Environment (getArgs)
 
-import           FF (loadActiveNotes)
+import           FF (getUtcToday, loadActiveNotes)
 import           FF.Config (Config (Config, dataDir), loadConfig)
 import           FF.Storage (runStorage)
+import           FF.Types (ModeMap (..), NoteView (NoteView, text), modeSelect,
+                           taskMode)
 
-import           NoteModel (NoteModel (super), addNote, new)
 import           Paths_ff_qt (version)
+
+data Storage = Storage FilePath (TVar LocalTime)
 
 main :: IO ()
 main = do
     Config { dataDir = Just dataDir } <- loadConfig
     timeVar                           <- newTVarIO =<< getRealLocalTime
+    let storage = Storage dataDir timeVar
     withApp $ \app -> do
         setOrganizationDomain app "ff.cblp.su"
         setOrganizationName   app "ff"
         setApplicationName    app "ff"
         setApplicationVersion app $ showVersion version
 
-        model <- NoteModel.new
-        runStorage dataDir timeVar loadActiveNotes >>= traverse_ (addNote model)
-
-        mainWindow <- mkMainWindow model
+        mainWindow <- newMainWindow storage
         QWidget.show mainWindow
         exec
 
 withApp :: (QApplication -> IO a) -> IO a
 withApp = withScopedPtr $ getArgs >>= QApplication.new
 
-mkMainWindow :: NoteModel -> IO QMainWindow
-mkMainWindow model = do
+newMainWindow :: Storage -> IO QMainWindow
+newMainWindow storage = do
     this <- QMainWindow.new
     setCentralWidget this =<< do
         tabs <- QTabWidget.new
-        addTab_ tabs "Agenda" =<< mkAgendaWidget model
+        addTab_ tabs "Agenda" =<< newAgendaWidget storage
         pure tabs
     setWindowTitle this "ff"
 
@@ -94,17 +95,34 @@ mkMainWindow model = do
 addTab_ :: QWidgetPtr widget => QTabWidget -> String -> widget -> IO ()
 addTab_ tabs name widget = void $ addTab tabs widget name
 
-mkAgendaWidget :: NoteModel -> IO QTreeView
-mkAgendaWidget model = do
-    this <- QTreeView.new
-    setAlternatingRowColors this True
-    setHeaderHidden         this True
-    setItemDelegate this =<< QStyledItemDelegate.new
-    setModel this (super model)
-    void $ onEvent this $ \(_ :: QShowEvent) -> do
-        setFocus this
-        setCurrentIndex this =<< index =<< item (super model) 0
+newAgendaWidget :: Storage -> IO QToolBox
+newAgendaWidget (Storage dataDir timeVar) = do
+    this <- QToolBox.new
+    void $ onEvent this $ \(_ :: QShowEvent) ->
+        -- TODO set the first item as current
         pure False
-    expandAll this
+
+    let newSection label = do
+            section <- QWidget.new
+            _ <- addItem this section label
+            QVBoxLayout.newWithParent section
+
+    overdue  <- newSection "Overdue"
+    endToday <- newSection "Due today"
+    endSoon  <- newSection "Due soon"
+    actual   <- newSection "Actual"
+    starting <- newSection "Starting soon"
+    let modeSections = ModeMap{..}
+
+    let addNote note@NoteView{text} = do
+            today <- getUtcToday
+            item <- newNoteWidgetWithText $ Text.unpack text
+            let sectionItem = modeSelect modeSections $ taskMode today note
+            addWidget sectionItem item
+
+    runStorage dataDir timeVar loadActiveNotes >>= traverse_ addNote
 
     pure this
+
+newNoteWidgetWithText :: String -> IO QLabel
+newNoteWidgetWithText = QLabel.newWithText
