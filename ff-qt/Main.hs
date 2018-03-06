e1f12b3edbb366941907ea3e55fd2204e1337c71
Yuriy Syrovetskiy <cblp@cblp.su>
2018-03-06 13:31:25 +0300

    Add note counters

--- ff-qt/Main.hs
+++ ff-qt/Main.hs
@@ -25,18 +25,19 @@ import           QSettings (new, setValue, value)
 import           QShowEvent (QShowEvent)
 import           QTabWidget (QTabWidget, addTab, new)
 import           Qtah.Event (onEvent)
-import           QToolBox (QToolBox, addItem, new)
+import           QToolBox (QToolBox, addItem, new, setItemText)
 import           QVariant (newWithByteArray, toByteArray)
 import           QVBoxLayout (newWithParent)
 import           QWidget (QWidgetPtr, new, restoreGeometry, saveGeometry,
-                          setWindowTitle, show)
+                          setWindowTitle)
+import qualified QWidget
 import           System.Environment (getArgs)
 
 import           FF (getUtcToday, loadActiveNotes)
 import           FF.Config (Config (Config, dataDir), loadConfig)
 import           FF.Storage (runStorage)
-import           FF.Types (ModeMap (..), NoteView (NoteView, text), modeSelect,
-                           taskMode)
+import           FF.Types (ModeMap (..), NoteView (NoteView, text),
+                           TaskMode (..), modeSelect, taskMode)
 
 import           Paths_ff_qt (version)
 
@@ -96,6 +97,27 @@ newMainWindow storage = do
 addTab_ :: QWidgetPtr widget => QTabWidget -> String -> widget -> IO ()
 addTab_ tabs name widget = void $ addTab tabs widget name
 
+sectionLabels :: ModeMap String
+sectionLabels = ModeMap
+    { overdue  = "Overdue"
+    , endToday = "Due today"
+    , endSoon  = "Due soon"
+    , actual   = "Actual"
+    , starting = "Starting soon"
+    }
+
+sectionIndices :: ModeMap Int
+sectionIndices = ModeMap
+    { overdue  = 0
+    , endToday = 1
+    , endSoon  = 2
+    , actual   = 3
+    , starting = 4
+    }
+
+sectionLabel :: TaskMode -> Int -> String
+sectionLabel mode n = concat [modeSelect sectionLabels mode, " (", show n, ")"]
+
 newAgendaWidget :: Storage -> IO QToolBox
 newAgendaWidget (Storage dataDir timeVar) = do
     this <- QToolBox.new
@@ -103,26 +125,33 @@ newAgendaWidget (Storage dataDir timeVar) = do
         -- TODO set the first item as current
         pure False
 
-    let newSection label = do
-            section <- QWidget.new
-            _ <- addItem this section label
-            box <- QVBoxLayout.newWithParent section
+    let newSection section = do
+            widget <- QWidget.new
+            _ <- addItem this widget label
+            box <- QVBoxLayout.newWithParent widget
             addStretch box
             pure box
+          where
+            label = sectionLabel section 0
 
-    overdue  <- newSection "Overdue"
-    endToday <- newSection "Due today"
-    endSoon  <- newSection "Due soon"
-    actual   <- newSection "Actual"
-    starting <- newSection "Starting soon"
+    overdue  <- newSection Overdue
+    endToday <- newSection EndToday
+    endSoon  <- newSection EndSoon
+    actual   <- newSection Actual
+    starting <- newSection Starting
     let modeSections = ModeMap{..}
 
     let addNote note@NoteView{text} = do
             today <- getUtcToday
             item <- newNoteWidgetWithText $ Text.unpack text
-            let section = modeSelect modeSections $ taskMode today note
-            n <- noteSectionCount section
-            insertWidget section n item
+            let mode = taskMode today note
+            let sectionBox = modeSelect modeSections mode
+            n <- sectionSize sectionBox
+            insertWidget sectionBox n item
+            setItemText
+                this
+                (modeSelect sectionIndices mode)
+                (sectionLabel mode $ n + 1)
 
     runStorage dataDir timeVar loadActiveNotes >>= traverse_ addNote
 
@@ -132,5 +161,5 @@ newNoteWidgetWithText :: String -> IO QLabel
 newNoteWidgetWithText = QLabel.newWithText
 
 -- Because last item is always a stretch.
-noteSectionCount :: QLayoutConstPtr layout => layout -> IO Int
-noteSectionCount layout = pred <$> count layout
+sectionSize :: QLayoutConstPtr layout => layout -> IO Int
+sectionSize layout = pred <$> count layout
