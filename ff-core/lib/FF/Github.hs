088c6056a33e468cec07625d140d3517cc84cbff
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-06-20 15:17:44 +0500

    Get proper task mode in `ff github list` Issue #10 PR #56

--- ff-core/lib/FF/Github.hs
+++ ff-core/lib/FF/Github.hs
@@ -9,28 +9,40 @@ module FF.Github
 
 import           Data.Foldable (toList)
 import           Data.List (genericLength)
+import           Data.List.Extra (groupSort)
+import           Data.Map.Strict (fromList)
 import           Data.Semigroup ((<>))
-import           Data.Time (UTCTime (..))
+import           Data.Time (Day, UTCTime (..))
+
 import           GitHub (Error, Id, Issue (..), IssueState (..), Milestone (..),
                          Name, Owner, Repo, URL (..), issueCreatedAt,
                          issueHtmlUrl, issueId, issueMilestone, issueState,
-                         issueTitle, stateOpen, untagId)
+                         issueTitle, untagId)
 import           GitHub.Endpoints.Issues (issuesForRepo)
 
 import           FF.Storage (DocId (..))
 import           FF.Types (ModeMap, NoteId, NoteView (..), Sample (..),
-                           Status (..), TaskMode (..), singletonSampleMap)
+                           Status (..), taskMode)
 
 runCmdGithub
-    :: Name Owner -> Name Repo -> Int -> IO (Either Error (ModeMap Sample))
-runCmdGithub owner repo limit =
-    fmap sampleMap <$> issuesForRepo owner repo stateOpen
-  where
-    sampleMap issues = singletonSampleMap Actual sample
-      where
-        sample = Sample (take limit nv) (genericLength nv)
-        nv = map toNoteView (toList issues)
+    :: Name Owner
+    -> Name Repo
+    -> Int
+    -> Day
+    -> IO (Either Error (ModeMap Sample))
+runCmdGithub owner repo limit today =
+    fmap (sampleMaps limit today) <$> issuesForRepo owner repo mempty
 
+sampleMaps :: Foldable t => Int -> Day -> t Issue -> ModeMap Sample
+sampleMaps limit today issues = fromList (takeFromMany limit groups)
+  where
+    nvs = map toNoteView (toList issues)
+    groups = groupSort [(taskMode today nv, nv) | nv <- nvs]
+    takeFromMany _ [] = []
+    takeFromMany lim ((mode, notes):gs) = (mode, Sample (take lim notes) (fromIntegral len))
+                            : takeFromMany (if lim <= len then 0 else lim - len) gs
+      where
+        len = genericLength notes
 
 toNoteView :: Issue -> NoteView
 toNoteView Issue{..} = NoteView
