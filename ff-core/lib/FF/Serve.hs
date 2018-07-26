6d5c74604caf2cae23b07a8893d07a340d342eb4
deflexor <deflexor@yandex.ru>
2018-07-26 12:56:39 +0300

    ff serve command: some styling for better appearance

--- ff-core/lib/FF/Serve.hs
+++ ff-core/lib/FF/Serve.hs
@@ -8,17 +8,22 @@
 module FF.Serve
     ( cmdServe ) where
 
+import           Control.Monad (when)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           Data.Maybe (isJust, fromJust)
 import qualified Data.Map.Strict as Map
+import qualified Data.Text as Text
 import           Web.Scotty (scotty, get, html)
+import           Text.Blaze.Html (stringValue)
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 import           FF (getUtcToday, getSamples)
 import           FF.Storage (runStorage)
 import qualified FF.Storage as Storage
 import           FF.Config (ConfigUI (..))
-import           FF.Types (ModeMap, Sample (..), TaskMode (..), omitted)
+import           FF.Types (NoteView(..), ModeMap, Sample (..), TaskMode (..), Tracked(..),  omitted)
 
 
 serveHttpPort :: Int
@@ -29,7 +34,10 @@ cmdServe h ui =
     liftIO $ scotty serveHttpPort $ get "/" $ do
         today <- getUtcToday
         nvs <- liftIO $ runStorage h $ getSamples ui Nothing today
-        html $ renderHtml $ prettyHtmlSamplesBySections nvs
+        html $ renderHtml $ do
+            H.meta H.! A.charset "utf-8"
+            H.style ".nfo-item * { margin: 2px; }"
+            prettyHtmlSamplesBySections nvs
 
 prettyHtmlSamplesBySections :: ModeMap Sample -> H.Html
 prettyHtmlSamplesBySections samples = do
@@ -44,8 +52,25 @@ prettyHtmlSample mode = \case
     Sample{notes} ->
         H.div $ do
             H.h1 $ H.toHtml (labels mode)
-            H.ul $ mconcat $ map (H.li . H.toHtml . show) notes
+            H.ul $ mconcat $ map fmtNote notes
   where
+    fmtNote (NoteView nid _ text start _ tracked) =
+        H.li $ do
+            H.h3 $ H.toHtml text
+            when (isJust nid) $ H.div H.! A.class_ "nfo-item" $ do
+                H.b "id"
+                H.toHtml $ show $ fromJust nid
+            H.div H.! A.class_ "nfo-item" $ do
+                H.b "start"
+                H.toHtml $ show start
+            when (isJust tracked) $ do
+                H.div H.! A.class_ "nfo-item" $ do
+                    H.b "tracking"
+                    H.toHtml $ trackedSource $ fromJust tracked
+                H.div H.! A.class_ "nfo-item" $ do
+                    let url = trackedUrl $ fromJust tracked
+                    H.b "url"
+                    H.a H.! A.href (stringValue $ Text.unpack url) $ H.toHtml url
     labels = \case
         Overdue n -> case n of
             1 -> "1 day overdue:"
