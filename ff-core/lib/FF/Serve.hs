650c88e7bf8bfe5807bd8c88bf898f3bf17f1715
deflexor <deflexor@yandex.ru>
2018-07-28 22:33:32 +0300

    ff serve command: some styling for better appearance

--- ff-core/lib/FF/Serve.hs
+++ ff-core/lib/FF/Serve.hs
@@ -16,7 +16,7 @@ import qualified Data.Text as Text
 import           Web.Scotty (scotty, get, html)
 import           Text.Blaze.Html (stringValue)
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
-import qualified Text.Blaze.Html5 as H
+import           Text.Blaze.Html5 (Html, meta, style, a, p, b, h1, h3, section, ul, li, toHtml, (!))
 import qualified Text.Blaze.Html5.Attributes as A
 
 import           FF (getUtcToday, getSamples)
@@ -35,42 +35,42 @@ cmdServe h ui =
         today <- getUtcToday
         nvs <- liftIO $ runStorage h $ getSamples ui Nothing today
         html $ renderHtml $ do
-            H.meta H.! A.charset "utf-8"
-            H.style ".nfo-item * { margin: 2px; }"
+            meta ! A.charset "utf-8"
+            style ".info-item * { margin: 2px; }"
             prettyHtmlSamplesBySections nvs
 
-prettyHtmlSamplesBySections :: ModeMap Sample -> H.Html
+prettyHtmlSamplesBySections :: ModeMap Sample -> Html
 prettyHtmlSamplesBySections samples = do
     mconcat [prettyHtmlSample mode sample | (mode, sample) <- Map.assocs samples]
-    mconcat [H.p $ H.toHtml numOmitted <> " task(s) omitted" | numOmitted > 0]
+    mconcat [p $ toHtml numOmitted <> " task(s) omitted" | numOmitted > 0]
   where
     numOmitted = sum $ fmap omitted samples
 
-prettyHtmlSample :: TaskMode -> Sample -> H.Html
+prettyHtmlSample :: TaskMode -> Sample -> Html
 prettyHtmlSample mode = \case
     Sample{total = 0} -> mempty
     Sample{notes} ->
-        H.div $ do
-            H.h1 $ H.toHtml (labels mode)
-            H.ul $ mconcat $ map fmtNote notes
-    where
+        section $ do
+            h1 $ toHtml (labels mode)
+            ul $ mconcat $ map fmtNote notes
+  where
     fmtNote (NoteView nid _ text start _ tracked) =
-        H.li $ do
-            H.h3 $ H.toHtml text
-            when (isJust nid) $ H.div H.! A.class_ "nfo-item" $ do
-                H.b "id"
-                H.toHtml $ show $ fromJust nid
-            H.div H.! A.class_ "nfo-item" $ do
-                H.b "start"
-                H.toHtml $ show start
+        li $ do
+            h3 $ toHtml text
+            when (isJust nid) $ section ! A.class_ "info-item" $ do
+                b "id"
+                toHtml $ show $ fromJust nid
+            section ! A.class_ "info-item" $ do
+                b "start"
+                toHtml $ show start
             when (isJust tracked) $ do
-                H.div H.! A.class_ "nfo-item" $ do
-                    H.b "tracking"
-                    H.toHtml $ trackedSource $ fromJust tracked
-                H.div H.! A.class_ "nfo-item" $ do
+                section ! A.class_ "info-item" $ do
+                    b "tracking"
+                    toHtml $ trackedSource $ fromJust tracked
+                section ! A.class_ "info-item" $ do
                     let url = trackedUrl $ fromJust tracked
-                    H.b "url"
-                    H.a H.! A.href (stringValue $ Text.unpack url) $ H.toHtml url
+                    b "url"
+                    a ! A.href (stringValue $ Text.unpack url) $ toHtml url
     labels = \case
         Overdue n -> case n of
             1 -> "1 day overdue:"
