e661ca6f056354017cdde5944ec58a82da02d671
Yuriy Syrovetskiy <cblp@cblp.su>
2019-02-15 17:05:07 +0300

    Extract MainWindow implementation

---
+++ ff-qt/MainWindow.cpp
@@ -0,0 +1,32 @@
+#include "MainWindow.hpp"
+
+
+MainWindow::MainWindow(StorageHandle storage):
+    agenda(new TaskListWidget(this, storage))
+{
+    // https://wiki.qt.io/Saving_Window_Size_State
+    QSettings settings;
+    restoreGeometry(settings.value("mainWindowGeometry").toByteArray());
+
+    auto tabs = new QTabWidget;
+    tabs->addTab(agenda, "Agenda");
+    setCentralWidget(tabs);
+    setWindowTitle("ff");
+    agenda->setFocus();
+
+    restoreState(settings.value("mainWindowState").toByteArray());
+}
+
+void MainWindow::upsertTask(Note note) {
+    // switch to this thread
+    QMetaObject::invokeMethod(this, [=]{
+        agenda->upsertTask(note);
+    });
+}
+
+void MainWindow::closeEvent(QCloseEvent *) {
+    // https://wiki.qt.io/Saving_Window_Size_State
+    QSettings settings;
+    settings.setValue("mainWindowGeometry", saveGeometry());
+    settings.setValue("mainWindowState", saveState());
+}
--- ff-qt/MainWindow.hpp
+++ ff-qt/MainWindow.hpp
@@ -9,42 +9,12 @@
 
 
 class MainWindow: public QMainWindow {
-
 private:
-
     TaskListWidget * agenda;
-
 public:
-
-    MainWindow(StorageHandle storage):
-        agenda(new TaskListWidget(this, storage))
-    {
-        // https://wiki.qt.io/Saving_Window_Size_State
-        QSettings settings;
-        restoreGeometry(settings.value("mainWindowGeometry").toByteArray());
-
-        auto tabs = new QTabWidget;
-        tabs->addTab(agenda, "Agenda");
-        setCentralWidget(tabs);
-        setWindowTitle("ff");
-        agenda->setFocus();
-
-        restoreState(settings.value("mainWindowState").toByteArray());
-    }
-
-    void upsertTask(Note note) {
-        // switch to this thread
-        QMetaObject::invokeMethod(this, [=]{
-            agenda->upsertTask(note);
-        });
-    }
-
-    void closeEvent(QCloseEvent *) override {
-        // https://wiki.qt.io/Saving_Window_Size_State
-        QSettings settings;
-        settings.setValue("mainWindowGeometry", saveGeometry());
-        settings.setValue("mainWindowState", saveState());
-    }
+    MainWindow(StorageHandle storage);
+    void upsertTask(Note note);
+    void closeEvent(QCloseEvent *) override;
 };
 
 
