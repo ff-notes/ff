d8c027a7b824f353075d0cca664a8fb06a4d63dc
Yuriy Syrovetskiy <cblp@cblp.su>
2019-03-13 18:19:02 +0300

    Use `bs-cstr` to get ASCIIZ

--- ff-qt/Main.hs
+++ ff-qt/Main.hs
@@ -10,13 +10,10 @@ module Main (main) where
 import           Prelude hiding (id)
 
 import           Control.Concurrent (forkIO)
-import           Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
 import           Data.Foldable (for_)
 import           Data.Maybe (isJust)
-import           Data.Text (Text)
 import qualified Data.Text as Text
-import qualified Data.Text.Encoding as Text
+import           Data.Text.Encoding (encodeUtf8)
 import           Data.Time (Day, toGregorian)
 import           Data.Typeable (cast)
 import           Data.Version (showVersion)
@@ -48,12 +45,12 @@ main = do
     dataDir <- getDataDir cfg
     storage <- Storage.newHandle dataDir
 
-    let version' = stringZ $ showVersion version
+    let version' = encodeUtf8 . Text.pack $ showVersion version
     storagePtr <- newStablePtr storage
 
     -- set up UI
     mainWindow <- [Cpp.exp| MainWindow * {
-        proxy_main($bs-ptr:version', $(StorageHandle storagePtr))
+        proxy_main($bs-cstr:version', $(StorageHandle storagePtr))
     }|]
 
     -- load current data to the view, asynchronously
@@ -76,32 +73,32 @@ upsertDocument storage mainWindow (CollectionDocId docid) = case docid of
 
 upsertTask :: Ptr MainWindow -> Entity Note -> IO ()
 upsertTask mainWindow Entity{entityId = DocId id, entityVal = note} = do
-    let id' = stringZ id
+    let id' = encodeUtf8 $ Text.pack id
         isActive = note_status == TaskStatus Active
         Note{note_text, note_start, note_end, note_track, note_status} = note
-        text = stringZ note_text
+        text = encodeUtf8 $ Text.pack note_text
         (startYear, startMonth, startDay) = toGregorianC note_start
         (endYear, endMonth, endDay) = maybe (0, 0, 0) toGregorianC note_end
         isTracking = isJust note_track
-        provider   = textZ $ foldMap track_provider   note_track
-        source     = textZ $ foldMap track_source     note_track
-        externalId = textZ $ foldMap track_externalId note_track
-        url        = textZ $ foldMap track_url        note_track
+        provider   = encodeUtf8 $ foldMap track_provider   note_track
+        source     = encodeUtf8 $ foldMap track_source     note_track
+        externalId = encodeUtf8 $ foldMap track_externalId note_track
+        url        = encodeUtf8 $ foldMap track_url        note_track
     [Cpp.block| void {
         MainWindow_upsertTask(
             $(MainWindow * mainWindow),
             (Note){
-                .id = $bs-ptr:id',
+                .id = $bs-cstr:id',
                 .isActive = $(bool isActive),
-                .text = $bs-ptr:text,
+                .text = $bs-cstr:text,
                 .start = {$(int startYear), $(int startMonth), $(int startDay)},
                 .end   = {$(int   endYear), $(int   endMonth), $(int   endDay)},
                 .isTracking = $(bool isTracking),
                 .track = {
-                    .provider   = $bs-ptr:provider,
-                    .source     = $bs-ptr:source,
-                    .externalId = $bs-ptr:externalId,
-                    .url        = $bs-ptr:url,
+                    .provider   = $bs-cstr:provider,
+                    .source     = $bs-cstr:source,
+                    .externalId = $bs-cstr:externalId,
+                    .url        = $bs-cstr:url,
                 },
             }
         );
@@ -110,11 +107,3 @@ upsertTask mainWindow Entity{entityId = DocId id, entityVal = note} = do
 toGregorianC :: Day -> (CInt, CInt, CInt)
 toGregorianC day = (y, m, d) where
     (fromIntegral -> y, fromIntegral -> m, fromIntegral -> d) = toGregorian day
-
--- | Zero-terminated C string from 'String'
-stringZ :: String -> ByteString
-stringZ = textZ . Text.pack
-
--- | Zero-terminated C string from 'Text'
-textZ :: Text -> ByteString
-textZ = (`BS.snoc` 0) . Text.encodeUtf8
