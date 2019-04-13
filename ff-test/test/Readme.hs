1f44813345c5c365976962d2015539f848733399
willbasky <vladislav.sabanov@gmail.com>
2019-04-13 09:27:22 +0500

    Refactor

--- ff-test/test/Readme.hs
+++ ff-test/test/Readme.hs
@@ -8,40 +8,36 @@ module Readme (readmeTest) where
 import           CMark (Node (..), NodeType (..), commonmarkToNode, optSafe)
 import           Data.Maybe (fromMaybe)
 import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 import           Hedgehog (Property, evalIO, property, (===))
 import           Test.Tasty (TestTree)
 import           Test.Tasty.Hedgehog (testProperty)
-import           Test.Tasty.TH (testGroupGenerator)
 
 import           FF.Options (showHelp)
 
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-
-
 readmeTest :: TestTree
-readmeTest = $(testGroupGenerator)
+readmeTest = testProperty "readme" prop_readme
 
 prop_readme :: Property
 prop_readme = property $ do
-    readme <- evalIO $ TIO.readFile "../README.md"
+    readme <- evalIO $ Text.readFile "../README.md"
     let readme' = fromMaybe noHelpSection $ parseMD readme
     ffhelp === readme'
 
 ffhelp :: Text
-ffhelp = T.concat [pref, "\n", T.pack $ showHelp Nothing, "\n"]
+ffhelp = Text.unlines [pref, Text.pack $ showHelp Nothing]
 
 parseMD :: Text -> Maybe Text
 parseMD s =
     let Node _ _ ns = commonmarkToNode [optSafe] s
         code = [block | Node _ (CODE_BLOCK _ block) _ <- ns]
-        help = filter ((==) pref . T.take (T.length pref)) code
-        help' = if null help then Nothing else Just (head help)
-    in  help'
+        help = filter ((==) pref . Text.take (Text.length pref)) code
+    in  if null help then Nothing else Just (head help)
 
 pref :: Text
 pref = "$ ff --help"
 
 noHelpSection :: Text
-noHelpSection = "There is no a help section in the README.md"
+noHelpSection = "There is no help section in the README.md"
 
