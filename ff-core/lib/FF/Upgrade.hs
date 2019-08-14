7e942d9a51c4f48bc8f8b2268dd7f0b632267516
Yuriy Syrovetskiy <cblp@cblp.su>
2019-08-14 19:00:35 +0300

    Style

--- ff-core/lib/FF/Upgrade.hs
+++ ff-core/lib/FF/Upgrade.hs
@@ -3,41 +3,52 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-module FF.Upgrade (upgradeDatabase) where
+module FF.Upgrade
+  ( upgradeDatabase
+    )
+where
 
-import           Data.Foldable (for_)
-import           RON.Event (getEventUuid)
-import           RON.Storage.Backend (MonadStorage, changeDocId, getCollections,
-                                      getDocuments)
-import           RON.Storage.FS (Collection, DocId, decodeDocId, docIdFromUuid,
-                                 modify)
-
-import           FF.Types (Note)
+import Data.Foldable (for_)
+import FF.Types (Note)
+import RON.Event (getEventUuid)
+import RON.Storage.Backend
+  ( MonadStorage,
+    changeDocId,
+    getCollections,
+    getDocuments
+    )
+import RON.Storage.FS
+  ( Collection,
+    DocId,
+    decodeDocId,
+    docIdFromUuid,
+    modify
+    )
 
 upgradeDatabase :: MonadStorage m => m ()
 upgradeDatabase = do
-    collections <- getCollections
-    for_ collections $ \case
-        "note"      -> upgradeCollection @Note
-        collection  -> fail $ "unsupported type " ++ show collection
+  collections <- getCollections
+  for_ collections $ \case
+    "note"     -> upgradeCollection @Note
+    collection -> fail $ "unsupported type " ++ show collection
 
-upgradeCollection :: forall a m . (Collection a, MonadStorage m) => m ()
+upgradeCollection :: forall a m. (Collection a, MonadStorage m) => m ()
 upgradeCollection = do
-    docs <- getDocuments @_ @a
-    for_ docs $ \docid -> do
-        docid' <- upgradeDocId docid
-        modify docid' $ pure ()
+  docs <- getDocuments @_ @a
+  for_ docs $ \docid -> do
+    docid' <- upgradeDocId docid
+    modify docid' $ pure ()
 
 upgradeDocId :: (Collection a, MonadStorage m) => DocId a -> m (DocId a)
 upgradeDocId docid = do
-    let mu = decodeDocId docid
-    case mu of
-        Just (True, _) -> pure docid
-        Just (False, uuid) -> do
-            let docid' = docIdFromUuid uuid
-            changeDocId docid docid'
-            pure docid'
-        Nothing -> do
-            docid' <- docIdFromUuid <$> getEventUuid
-            changeDocId docid docid'
-            pure docid'
+  let mu = decodeDocId docid
+  case mu of
+    Just (True, _) -> pure docid
+    Just (False, uuid) -> do
+      let docid' = docIdFromUuid uuid
+      changeDocId docid docid'
+      pure docid'
+    Nothing -> do
+      docid' <- docIdFromUuid <$> getEventUuid
+      changeDocId docid docid'
+      pure docid'
