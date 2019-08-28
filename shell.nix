0e111fdb191014ef6f3c8bfee1334bf2732758e6
Sergey Makarov <setser200018@gmail.com>
2019-08-28 23:45:15 +0300

    Remove src section from shell.nix

--- shell.nix
+++ shell.nix
@@ -3,12 +3,6 @@ let ff-pkg = { buildStackProject, fetchgit, gmp, zlib, qt5, wrapGAppsHook }:
 buildStackProject {
   name = "ff";
 
-  src = fetchgit {
-    url = "https://github.com/ff-notes/ff";
-    rev = "18492f6067720977c25cc012df88fdeb9ffee5cc";
-    sha256 = "1c0zd97bl8i7795n8vhfgbg6y7dgqkby279mngfadg2ich28axrr";
-  };
-
   buildInputs = [ gmp zlib qt5.qtbase wrapGAppsHook ];
 
   shellHook = "unset STACK_IN_NIX_SHELL";
