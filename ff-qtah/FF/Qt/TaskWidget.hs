0baadf33336265defd0c4f0bea56abae6f9ab41d
Yuriy Syrovetskiy <cblp@cblp.su>
2019-12-21 20:43:37 +0300

    fixup! fixup! ff-qtah: Make task widget scrollable

--- ff-qtah/FF/Qt/TaskWidget.hs
+++ ff-qtah/FF/Qt/TaskWidget.hs
@@ -13,6 +13,7 @@ import           Graphics.UI.Qtah.Core.QObject (QObjectConstPtr, QObjectPtr,
 import qualified Graphics.UI.Qtah.Core.QObject as QObject
 import           Graphics.UI.Qtah.Core.Types (QtAlignmentFlag (AlignTop))
 import qualified Graphics.UI.Qtah.Widgets.QBoxLayout as QBoxLayout
+import           Graphics.UI.Qtah.Widgets.QFrame (QFrame)
 import qualified Graphics.UI.Qtah.Widgets.QFrame as QFrame
 import           Graphics.UI.Qtah.Widgets.QLabel (QLabel)
 import qualified Graphics.UI.Qtah.Widgets.QLabel as QLabel
@@ -22,7 +23,6 @@ import qualified Graphics.UI.Qtah.Widgets.QVBoxLayout as QVBoxLayout
 import           Graphics.UI.Qtah.Widgets.QWidget (QWidgetConstPtr, QWidgetPtr,
                                                    toQWidget, toQWidgetConst)
 import qualified Graphics.UI.Qtah.Widgets.QWidget as QWidget
--- import qualified Graphics.UI.Qtah.Widgets.QWidget as QWidget
 import           RON.Storage.FS (runStorage)
 import qualified RON.Storage.FS as Storage
 
@@ -34,11 +34,16 @@ import           FF.Types (Entity (..), Note (Note, note_text), NoteId,
 -- package
 import qualified FF.Qt.DateComponent as DateComponent
 
-data TaskWidget =
-  TaskWidget{super :: QScrollArea, label :: QLabel, storage :: Storage.Handle}
+data TaskWidget = TaskWidget
+  { super   :: QScrollArea
+  , frame   :: QFrame
+  , label   :: QLabel
+  , storage :: Storage.Handle
+  }
 
 instance CppPtr TaskWidget where
-  nullptr = TaskWidget{super = nullptr, label = nullptr, storage = undefined}
+  nullptr = TaskWidget
+    {super = nullptr, frame = nullptr, label = nullptr, storage = undefined}
   withCppPtr TaskWidget{super} proc = withCppPtr super $ proc . castPtr
   toPtr = castPtr . toPtr . super
   touchCppPtr = touchCppPtr . super
@@ -58,42 +63,29 @@ instance QWidgetPtr TaskWidget where
 new :: Storage.Handle -> IO TaskWidget
 new storage = do
   super <- QScrollArea.new
-  QObject.setObjectName super "TaskWidget.super : QScrollArea"
-  -- QFrame.setFrameShadow super QFrame.Sunken
-  -- QFrame.setFrameShape  super QFrame.Box
-  -- QFrame.setLineWidth   super 10
 
-  -- frame <- QFrame.new
-  -- QObject.setObjectName frame "TaskWidget.frame : QFrame"
-  -- QFrame.setFrameShadow frame QFrame.Sunken
-  -- QFrame.setFrameShape  frame QFrame.Box
-  -- QFrame.setLineWidth   frame 10
-  -- QScrollArea.setWidget super frame
+  frame <- QFrame.new
+  QScrollArea.setWidget super frame
 
   label <- QLabel.new
   QObject.setObjectName label "TaskWidget.label : QLabel"
   QLabel.setAlignment label AlignTop
   QLabel.setWordWrap  label True
-  QScrollArea.setWidget super label
 
-  -- start <- DateComponent.new "Start:"
-  -- QObject.setObjectName start "TaskWidget.start : DateComponent"
+  start <- DateComponent.new "Start:"
+  end   <- DateComponent.new "Deadline:"
 
-  -- end   <- DateComponent.new "Deadline:"
-  -- QObject.setObjectName end "TaskWidget.end : DateComponent"
+  box <- QVBoxLayout.newWithParent frame
+  QBoxLayout.addWidget box label
+  QBoxLayout.addLayout box start
+  QBoxLayout.addLayout box end
 
-  -- box <- QVBoxLayout.newWithParent frame
-  -- QObject.setObjectName box "TaskWidget.box : QVBoxLayout"
-  -- QBoxLayout.addWidget box label
-  -- QBoxLayout.addLayout box start
-  -- QBoxLayout.addLayout box end
-
-  pure TaskWidget{super, label, storage}
+  pure TaskWidget{super, frame, label, storage}
 
 update :: TaskWidget -> NoteId -> IO ()
-update TaskWidget{label, storage} noteId = do
+update TaskWidget{frame, label, storage} noteId = do
   Entity{entityVal} <- runStorage storage $ loadNote noteId >>= viewNote
   let NoteView{note} = entityVal
   let Note{note_text} = note
   QLabel.setText label $ fromRgaM note_text
-  QWidget.adjustSize label
+  QWidget.adjustSize frame
