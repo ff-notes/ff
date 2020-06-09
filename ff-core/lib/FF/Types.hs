87ea5c5f374294ffeffd8bb022539c9a1ba9a8fc
Yuriy Syrovetskiy <cblp@cblp.su>
2020-06-09 23:34:35 +0300

    Style

--- ff-core/lib/FF/Types.hs
+++ ff-core/lib/FF/Types.hs
@@ -17,61 +17,48 @@
 
 module FF.Types where
 
+import           Control.Monad ((>=>))
+import           Control.Monad.Except (throwError)
+import           Control.Monad.Reader (ask, runReaderT)
 import qualified CRDT.Cv.RGA as CRDT
-import qualified CRDT.LWW as CRDT
 import qualified CRDT.LamportClock as CRDT
-import Control.Monad ((>=>))
-import Control.Monad.Except (throwError)
-import Control.Monad.Reader (ask, runReaderT)
-import Data.Aeson ((.:), (.:?), FromJSON, eitherDecode, parseJSON, withObject)
+import qualified CRDT.LWW as CRDT
+import           Data.Aeson (FromJSON, eitherDecode, parseJSON, withObject,
+                             (.:), (.:?))
 import qualified Data.Aeson as JSON
-import Data.Aeson.TH (defaultOptions, deriveFromJSON)
-import Data.Aeson.Types (parseEither)
-import Data.ByteString.Lazy (ByteString)
-import Data.Hashable (Hashable)
-import Data.List (genericLength)
-import Data.Map.Strict (Map)
+import           Data.Aeson.TH (defaultOptions, deriveFromJSON)
+import           Data.Aeson.Types (parseEither)
+import           Data.ByteString.Lazy (ByteString)
+import           Data.Hashable (Hashable)
+import           Data.List (genericLength)
+import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe (fromJust, maybeToList)
-import Data.Set (Set)
-import Data.Text (Text)
-import Data.Time (diffDays)
-import FF.CrdtAesonInstances ()
-import GHC.Generics (Generic)
-import Numeric.Natural (Natural)
-import RON.Data
-  ( MonadObjectState,
-    Replicated (encoding),
-    ReplicatedAsPayload (fromPayload, toPayload),
-    evalObjectState,
-    payloadEncoding,
-    readObject,
-    stateFromChunk,
-    stateToWireChunk,
-  )
-import RON.Data.LWW (lwwType)
-import RON.Data.RGA (RgaRep)
-import RON.Data.Time (Day)
-import RON.Epoch (localEpochTimeFromUnix)
-import RON.Error (Error (Error), MonadE, liftEitherString)
-import RON.Event (Event (Event), applicationSpecific, encodeEvent)
-import RON.Schema.TH (mkReplicated)
-import RON.Storage
-  ( Collection,
-    DocId,
-    collectionName,
-    fallbackParse,
-    loadDocument,
-  )
-import RON.Storage.Backend (Document (Document, objectFrame), MonadStorage)
-import RON.Types
-  ( Atom (AUuid),
-    ObjectFrame (ObjectFrame, frame, uuid),
-    ObjectRef (ObjectRef),
-    Op (Op),
-    UUID,
-    WireStateChunk (WireStateChunk, stateBody, stateType),
-  )
+import           Data.Maybe (fromJust, maybeToList)
+import           Data.Set (Set)
+import           Data.Text (Text)
+import           Data.Time (diffDays)
+import           FF.CrdtAesonInstances ()
+import           GHC.Generics (Generic)
+import           Numeric.Natural (Natural)
+import           RON.Data (MonadObjectState, Replicated (encoding),
+                           ReplicatedAsPayload (fromPayload, toPayload),
+                           evalObjectState, payloadEncoding, readObject,
+                           stateFromChunk, stateToWireChunk)
+import           RON.Data.LWW (lwwType)
+import           RON.Data.RGA (RgaRep)
+import           RON.Data.Time (Day)
+import           RON.Epoch (localEpochTimeFromUnix)
+import           RON.Error (Error (Error), MonadE, liftEitherString)
+import           RON.Event (Event (Event), applicationSpecific, encodeEvent)
+import           RON.Schema.TH (mkReplicated)
+import           RON.Storage (Collection, DocId, collectionName, fallbackParse,
+                              loadDocument)
+import           RON.Storage.Backend (Document (Document, objectFrame),
+                                      MonadStorage)
+import           RON.Types (Atom (AUuid),
+                            ObjectFrame (ObjectFrame, frame, uuid),
+                            ObjectRef (ObjectRef), Op (Op), UUID,
+                            WireStateChunk (WireStateChunk, stateBody, stateType))
 import qualified RON.UUID as UUID
 
 data NoteStatus = TaskStatus Status | Wiki
@@ -192,9 +179,7 @@ type ContactId = DocId Contact
 type TagId = DocId Tag
 
 instance Collection Note where
-
   collectionName = "note"
-
   fallbackParse = parseNoteV1
 
 instance Collection Contact where
@@ -203,7 +188,7 @@ instance Collection Contact where
 instance Collection Tag where
   collectionName = "tag"
 
-data Sample a = Sample {items :: [a], total :: Natural}
+data Sample a = Sample{items :: [a], total :: Natural}
   deriving (Eq, Functor, Show)
 
 {- |
@@ -211,7 +196,7 @@ data Sample a = Sample {items :: [a], total :: Natural}
   Should not be used directly, use 'EntityDoc' or 'EntityView' instead.
 -}
 data Entity doc val
-  = Collection doc => Entity {entityId :: DocId doc, entityVal :: val}
+  = Collection doc => Entity{entityId :: DocId doc, entityVal :: val}
 
 deriving instance Eq val => Eq (Entity doc val)
 
@@ -226,19 +211,18 @@ type ContactSample = Sample (EntityDoc Contact)
 type NoteSample = Sample (EntityView Note)
 
 emptySample :: Sample a
-emptySample = Sample {items = [], total = 0}
+emptySample = Sample{items = [], total = 0}
 
 -- | Number of notes omitted from the sample.
 omitted :: Sample a -> Natural
-omitted Sample {total, items} = total - genericLength items
+omitted Sample{total, items} = total - genericLength items
 
 data family View doc
 
-data instance View Note
-  = NoteView
-      { note :: Note,
-        tags :: Set Text
-      }
+data instance View Note = NoteView
+  { note :: Note
+  , tags :: Set Text
+  }
   deriving (Eq, Show)
 
 type ModeMap = Map TaskMode
@@ -254,29 +238,30 @@ data TaskMode
 
 taskModeOrder :: TaskMode -> Int
 taskModeOrder = \case
-  Overdue _ -> 0
-  EndToday -> 1
-  EndSoon _ -> 2
-  Actual -> 3
+  Overdue  _ -> 0
+  EndToday   -> 1
+  EndSoon  _ -> 2
+  Actual     -> 3
   Starting _ -> 4
 
 instance Ord TaskMode where
-  Overdue n <= Overdue m = n >= m
-  EndSoon n <= EndSoon m = n <= m
-  Starting n <= Starting m = n <= m
-  m1 <= m2 = taskModeOrder m1 <= taskModeOrder m2
+  Overdue  n <= Overdue  m = n               >= m
+  EndSoon  n <= EndSoon  m = n               <= m
+  Starting n <= Starting m = n               <= m
+  n          <= m          = taskModeOrder n <= taskModeOrder m
 
 taskMode :: Day -> Note -> TaskMode
-taskMode today Note {note_start, note_end} = case note_end of
-  Nothing
-    | start <= today -> Actual
-    | otherwise -> starting start today
-  Just e -> case compare e today of
-    LT -> overdue today e
-    EQ -> EndToday
-    GT
-      | start <= today -> endSoon e today
-      | otherwise -> starting start today
+taskMode today Note{note_start, note_end} =
+  case note_end of
+    Nothing
+      | start <= today -> Actual
+      | otherwise      -> starting start today
+    Just e -> case compare e today of
+      LT -> overdue today e
+      EQ -> EndToday
+      GT
+        | start <= today -> endSoon e today
+        | otherwise      -> starting start today
   where
     start = fromJust note_start
     overdue = helper Overdue
@@ -289,20 +274,20 @@ type Limit = Natural
 -- * Legacy, v2
 loadNote :: MonadStorage m => NoteId -> m (EntityDoc Note)
 loadNote entityId = do
-  Document {objectFrame} <- loadDocument entityId
+  Document{objectFrame} <- loadDocument entityId
   let tryCurrentEncoding = evalObjectState objectFrame readObject
   case tryCurrentEncoding of
-    Right entityVal -> pure Entity {entityId, entityVal}
+    Right entityVal -> pure Entity{entityId, entityVal}
     Left e1 -> do
       let tryNote2Encoding = evalObjectState objectFrame readNoteFromV2
       case tryNote2Encoding of
-        Right entityVal -> pure Entity {entityId, entityVal}
+        Right entityVal -> pure Entity{entityId, entityVal}
         Left e2 -> throwError $ Error "loadNote" [e1, e2]
 
 readNoteFromV2 :: (MonadE m, MonadObjectState a m) => m Note
 readNoteFromV2 = do
   ObjectRef uuid <- ask
-  NoteV2 {..} <- runReaderT readObject (ObjectRef @NoteV2 uuid)
+  NoteV2{..} <- runReaderT readObject (ObjectRef @NoteV2 uuid)
   pure Note
     { note_end = noteV2_end,
       note_start = noteV2_start,
@@ -314,7 +299,7 @@ readNoteFromV2 = do
     }
 
 trackFromV2 :: TrackV2 -> Track
-trackFromV2 TrackV2 {..} =
+trackFromV2 TrackV2{..} =
   Track
     { track_externalId = trackV2_externalId,
       track_provider = trackV2_provider,
@@ -369,8 +354,8 @@ parseNoteV1 objectId = liftEitherString . (eitherDecode >=> parseEither p)
                   (textId, stateToWireChunk $ rgaFromV1 text) -- rgaType
                 ]
                 ++ maybeToList mTrackObject
-      pure ObjectFrame {uuid = objectId, frame}
-    mkLww stateBody = WireStateChunk {stateType = lwwType, stateBody}
+      pure ObjectFrame{uuid = objectId, frame}
+    mkLww stateBody = WireStateChunk{stateType = lwwType, stateBody}
     textId = UUID.succValue objectId
     trackId = UUID.succValue textId
     endName = $(UUID.liftName "end")
