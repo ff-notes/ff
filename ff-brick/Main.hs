bae27b6a0f43a05d11e3b9df707800c5b6afa6d9
Yuriy Syrovetskiy <git-orbi@cblp.su>
2025-09-03 22:48:23 +0200

    Add simple list visualization

--- ff-brick/Main.hs
+++ ff-brick/Main.hs
@@ -1,2 +1,75 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedLabels #-}
+
+import Brick (
+    App (App),
+    AttrMap,
+    BrickEvent (VtyEvent),
+    EventM,
+    Widget,
+    attrMap,
+    defaultMain,
+    halt,
+    neverShowCursor,
+    on,
+    str,
+    withAttr,
+    zoom,
+    (<=>),
+ )
+import Brick qualified
+import Brick.Widgets.Border (borderWithLabel)
+import Brick.Widgets.List (
+    List,
+    handleListEvent,
+    list,
+    listSelectedAttr,
+    renderList,
+ )
+import Control.Monad (void)
+import Data.Generics.Labels ()
+import Data.Vector qualified as Vector
+import GHC.Generics (Generic)
+import Graphics.Vty (Event (EvKey), Key (KEsc), black, defAttr, white)
+
 main :: IO ()
-main = pure ()
+main = void $ defaultMain app initialModel
+
+newtype Model = Model {notes :: List () String} deriving (Generic)
+
+initialModel :: Model
+initialModel =
+    Model{notes = list () (Vector.fromList ["3", "15", "9", "20"]) 10}
+
+app :: App Model () ()
+app =
+    App
+        { appAttrMap = const appAttrMap
+        , appChooseCursor = neverShowCursor
+        , appDraw
+        , appHandleEvent
+        , appStartEvent = pure ()
+        }
+
+appAttrMap :: AttrMap
+appAttrMap = attrMap defAttr [(listSelectedAttr, black `on` white)]
+
+appDraw :: Model -> [Widget ()]
+appDraw Model{notes} =
+    [ borderWithLabel (str "Agenda") (renderList renderListItem True notes)
+        <=> str "Esc - exit"
+    ]
+
+appHandleEvent :: BrickEvent () () -> EventM () Model ()
+appHandleEvent = \case
+    VtyEvent e -> appHandleVtyEvent e
+    _ -> pure ()
+
+appHandleVtyEvent :: Event -> EventM () Model ()
+appHandleVtyEvent = \case
+    EvKey KEsc _ -> halt
+    e -> zoom #notes $ handleListEvent e
+
+renderListItem :: Bool -> String -> Widget ()
+renderListItem isSelected item =
+    (if isSelected then withAttr listSelectedAttr else id) $ str item
