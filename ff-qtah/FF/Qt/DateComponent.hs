dbff07c0049efc20346467de25a7db603740d2dc
Yuriy Syrovetskiy <git-orbi@cblp.su>
2026-03-17 12:13:58 +0100

    Style

--- ff-qtah/FF/Qt/DateComponent.hs
+++ ff-qtah/FF/Qt/DateComponent.hs
@@ -3,71 +3,90 @@
 module FF.Qt.DateComponent (DateComponent, new) where
 
 -- global
-import           Foreign (castPtr)
-import           Foreign.Hoppy.Runtime (CppPtr, nullptr, toPtr, touchCppPtr,
-                                        withCppPtr)
-import           Graphics.UI.Qtah.Core.QObject (QObjectConstPtr, QObjectPtr,
-                                                toQObject, toQObjectConst)
-import qualified Graphics.UI.Qtah.Widgets.QAbstractSpinBox as QAbstractSpinBox
-import qualified Graphics.UI.Qtah.Widgets.QBoxLayout as QBoxLayout
-import           Graphics.UI.Qtah.Widgets.QDateEdit (QDateEdit)
-import qualified Graphics.UI.Qtah.Widgets.QDateEdit as QDateEdit
-import qualified Graphics.UI.Qtah.Widgets.QDateTimeEdit as QDateTimeEdit
-import           Graphics.UI.Qtah.Widgets.QHBoxLayout (QHBoxLayout)
-import qualified Graphics.UI.Qtah.Widgets.QHBoxLayout as QHBoxLayout
-import           Graphics.UI.Qtah.Widgets.QLabel (QLabel)
-import qualified Graphics.UI.Qtah.Widgets.QLabel as QLabel
-import           Graphics.UI.Qtah.Widgets.QLayout (QLayoutConstPtr, QLayoutPtr,
-                                                   toQLayout, toQLayoutConst)
-import           Graphics.UI.Qtah.Widgets.QLayoutItem (QLayoutItemConstPtr,
-                                                       QLayoutItemPtr,
-                                                       toQLayoutItem,
-                                                       toQLayoutItemConst)
+import Foreign (castPtr)
+import Foreign.Hoppy.Runtime (
+    CppPtr,
+    nullptr,
+    toPtr,
+    touchCppPtr,
+    withCppPtr,
+ )
+import Graphics.UI.Qtah.Core.QObject (
+    QObjectConstPtr,
+    QObjectPtr,
+    toQObject,
+    toQObjectConst,
+ )
+import Graphics.UI.Qtah.Widgets.QAbstractSpinBox qualified as QAbstractSpinBox
+import Graphics.UI.Qtah.Widgets.QBoxLayout qualified as QBoxLayout
+import Graphics.UI.Qtah.Widgets.QDateEdit (QDateEdit)
+import Graphics.UI.Qtah.Widgets.QDateEdit qualified as QDateEdit
+import Graphics.UI.Qtah.Widgets.QDateTimeEdit qualified as QDateTimeEdit
+import Graphics.UI.Qtah.Widgets.QHBoxLayout (QHBoxLayout)
+import Graphics.UI.Qtah.Widgets.QHBoxLayout qualified as QHBoxLayout
+import Graphics.UI.Qtah.Widgets.QLabel (QLabel)
+import Graphics.UI.Qtah.Widgets.QLabel qualified as QLabel
+import Graphics.UI.Qtah.Widgets.QLayout (
+    QLayoutConstPtr,
+    QLayoutPtr,
+    toQLayout,
+    toQLayoutConst,
+ )
+import Graphics.UI.Qtah.Widgets.QLayoutItem (
+    QLayoutItemConstPtr,
+    QLayoutItemPtr,
+    toQLayoutItem,
+    toQLayoutItemConst,
+ )
 
-data DateComponent =
-  DateComponent{super :: QHBoxLayout, label :: QLabel, dateEdit :: QDateEdit}
+data DateComponent = DateComponent
+    { super :: QHBoxLayout
+    , label :: QLabel
+    , dateEdit :: QDateEdit
+    }
 
 instance CppPtr DateComponent where
-  nullptr = DateComponent{super = nullptr, label = nullptr, dateEdit = nullptr}
-  withCppPtr DateComponent{super} proc = withCppPtr super $ proc . castPtr
-  toPtr = castPtr . toPtr . super
-  touchCppPtr = touchCppPtr . super
+    nullptr =
+        DateComponent{super = nullptr, label = nullptr, dateEdit = nullptr}
+    withCppPtr DateComponent{super} proc = withCppPtr super $ proc . castPtr
+    toPtr = castPtr . toPtr . super
+    touchCppPtr = touchCppPtr . super
 
 instance QObjectConstPtr DateComponent where
-  toQObjectConst = toQObjectConst . super
+    toQObjectConst = toQObjectConst . super
 
 instance QObjectPtr DateComponent where
-  toQObject = toQObject . super
+    toQObject = toQObject . super
 
 instance QLayoutItemConstPtr DateComponent where
-  toQLayoutItemConst = toQLayoutItemConst . super
+    toQLayoutItemConst = toQLayoutItemConst . super
 
 instance QLayoutItemPtr DateComponent where
-  toQLayoutItem = toQLayoutItem . super
+    toQLayoutItem = toQLayoutItem . super
 
 instance QLayoutConstPtr DateComponent where
-  toQLayoutConst = toQLayoutConst . super
+    toQLayoutConst = toQLayoutConst . super
 
 instance QLayoutPtr DateComponent where
-  toQLayout = toQLayout . super
+    toQLayout = toQLayout . super
 
 new :: String -> IO DateComponent
 new title = do
-  super <- QHBoxLayout.new
+    super <- QHBoxLayout.new
 
-  label <- QLabel.newWithText title
-  QBoxLayout.addWidget super label
+    label <- QLabel.newWithText title
+    QBoxLayout.addWidget super label
 
-  dateEdit <- QDateEdit.new
-  QDateTimeEdit.setCalendarPopup dateEdit True
-  QBoxLayout.addWidget super dateEdit
+    dateEdit <- QDateEdit.new
+    QDateTimeEdit.setCalendarPopup dateEdit True
+    QBoxLayout.addWidget super dateEdit
 
-  QBoxLayout.addStretch super
+    QBoxLayout.addStretch super
 
-  let this = DateComponent{super, label, dateEdit}
-  setEditable this False
-  pure this
+    let this = DateComponent{super, label, dateEdit}
+    setEditable this False
+    pure this
 
 setEditable :: DateComponent -> Bool -> IO ()
 setEditable DateComponent{dateEdit} editable =
-  QAbstractSpinBox.setReadOnly dateEdit $ not editable
+    QAbstractSpinBox.setReadOnly dateEdit $ not editable
